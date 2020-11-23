d6c2b9b5774f593ee1835d68455af1c323468927
GitKokoszka <dobrzeniewskia@gmail.com>
2020-11-23 14:18:19 +0100

    Remove HttpsRedirection

--- Cookify.API/Startup.cs
+++ Cookify.API/Startup.cs
@@ -44,7 +44,7 @@ namespace Cookify.API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cookify.API v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 

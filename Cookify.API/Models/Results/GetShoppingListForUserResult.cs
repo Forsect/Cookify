d773e93cb236e09d3efb77d8b9a226273378b65c
GitKokoszka <dobrzeniewskia@gmail.com>
2021-01-26 00:30:52 +0100

    API: Add and Remove product in shopping list

---
+++ Cookify.API/Models/Results/GetShoppingListForUserResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookify.API.Models.Results
+{
+    public class GetShoppingListForUserResult
+    {
+        public string UserLogin { get; set; }
+        public IEnumerable<string> ShoppingList { get; set; }
+    }
+}
---
+++ Cookify.API/Utilities/JwtHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Cookify.API;
+using Cookify.API.Models.Repository;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Clavis.API.Utilities
+{
+    public static class JwtHelper
+    {
+        public static bool IsJwtValid(string jwtToken, string tokenKey, string issuer, out User user)
+        {
+            user = null;
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(jwtToken))
+                {
+                    return false;
+                }
+
+                var tokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = GetSymmetricSecurityKey(tokenKey),
+                    ValidateLifetime = false,
+                    ValidateAudience = false,
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var claims = tokenHandler.ValidateToken(jwtToken, tokenValidationParameters, out var securityInfo);
+
+                user = new User
+                {
+                    Id = claims.FindFirstValue(ClaimNames.Id),
+                    Login = claims.FindFirstValue(ClaimNames.Login)
+                };
+
+                return !JwtExpired(claims);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static SecurityKey GetSymmetricSecurityKey(string secretKey)
+        {
+            byte[] symmetricKey = Encoding.ASCII.GetBytes(secretKey);
+            return new SymmetricSecurityKey(symmetricKey);
+        }
+
+        private static bool JwtExpired(ClaimsPrincipal claims)
+        {
+            string expirationTimestampString = claims.Claims.FirstOrDefault(x => x.Type == "exp")?.Value;
+
+            if (string.IsNullOrWhiteSpace(expirationTimestampString))
+            {
+                return true;
+            }
+
+            var offset = DateTimeOffset.FromUnixTimeSeconds(long.Parse(expirationTimestampString));
+
+            return offset.DateTime < DateTime.UtcNow;
+        }
+    }
+}

47f5c0ada6eff70c4c1f6dbdf364193344316db3
GitKokoszka <dobrzeniewskia@gmail.com>
2021-01-30 22:57:59 +0100

    FRONT: Fix api url

--- cookify_front/src/shared/constants/Constants.ts
+++ cookify_front/src/shared/constants/Constants.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "https://localhost:44332/api";
+export const BASE_API_URL = "https://cookify-api.herokuapp.com/api";
 export const JWT_LOCAL_STORAGE_KEY = "JWT";
 
 export const CAPTCHA_KEY = "6LeX1icaAAAAANGk13IeLaRc_aKHclgJuPtyxU6E";

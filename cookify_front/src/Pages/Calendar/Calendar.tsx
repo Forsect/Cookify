85fb84be9688c627de361cc6bec9e39401961aa0
GitKokoszka <dobrzeniewskia@gmail.com>
2021-02-01 01:21:53 +0100

    FRONT: Calendar axios ready

--- cookify_front/src/Pages/Calendar/Calendar.tsx
+++ cookify_front/src/Pages/Calendar/Calendar.tsx
@@ -44,10 +44,7 @@ const Calendar: React.FC<CalendarProps> = observer((props: CalendarProps) => {
   }, []);
 
   const renderItem = (day: Date, index: number) => {
-    // console.log(typeof day);
-    // console.dir(day);
-    // console.dir(mealsStore.dailyMealsList);
-    console.dir(mealsStore.dailyMealsList.find((x) => isSameDay(x.date, day)));
+    console.dir(mealsStore.dailyMealsList);
     return (
       <SingleCalendarItem
         isSelected={props.selectedDays.some((x) => isSameDay(day, x.date))}
@@ -126,6 +123,12 @@ const Calendar: React.FC<CalendarProps> = observer((props: CalendarProps) => {
     <SelectedDay
       onClose={() => {
         setSelectedDay(null);
+        // if (
+        //   selectedElementRef.current !== null &&
+        //   containerRef.current !== null
+        // ) {
+        //   containerRef.current.scrollTop = selectedElementRef.current.scrollTop;
+        // }
       }}
       onDelete={(dailyMeal) => {
         mealsStore.removeDailyMeal(dailyMeal);
